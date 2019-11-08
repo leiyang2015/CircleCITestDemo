1e5d219b9e4b04a612c02cfb97623401d8975279
yanglei <yanglei@basefex.com>
2019-11-08 16:03:24 +0800

    project init

---
+++ CircleCITestDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CircleCITestDemo
+//
+//  Created by yanglei on 2019/11/8.
+//  Copyright © 2019 yanglei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        debugPrint("CircleCITestDemo viewDidLoad")
+        
+        
+    }
+
+
+}
+
